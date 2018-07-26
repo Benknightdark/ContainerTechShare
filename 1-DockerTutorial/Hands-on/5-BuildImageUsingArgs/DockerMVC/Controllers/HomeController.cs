8e3b2450eb75e3d6cbc4fcc21d3216ec167003ec
Ben <moon791017@gmail.com>
2018-07-26 09:36:30 +0800

    OK

--- 1-DockerTutorial/Hands-on/5-BuildImageUsingArgs/DockerMVC/Controllers/HomeController.cs
+++ 1-DockerTutorial/Hands-on/5-BuildImageUsingArgs/DockerMVC/Controllers/HomeController.cs
@@ -3,41 +3,47 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using DockerMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
-namespace DockerMVC.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace DockerMVC.Controllers {
+    public class HomeController : Controller {
+        public IConfiguration Configuration { get; }
+        public HomeController (IConfiguration configuration) {
+            Configuration = configuration;
+
+        }
+        public IActionResult Index () {
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
+            return View ();
         }
 
-        public IActionResult About()
-        {
+        public IActionResult About () {
             ViewData["Message"] = "Your application description page.";
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
 
-            return View();
+            return View ();
         }
 
-        public IActionResult Contact()
-        {
+        public IActionResult Contact () {
             ViewData["Message"] = "Your contact page.";
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
 
-            return View();
+            return View ();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        public IActionResult Privacy () {
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
+
+            return View ();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
+
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
