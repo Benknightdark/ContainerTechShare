bfcd3164129d7e7656c40dc6623cebf78df99b81
BenKung <moon791017@gmail.com>
2018-07-31 17:28:25 +0800

    OK

---
+++ 2-k8sTutorial/Hands-On/6-LoadBalancerService/DockerMVC/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DockerMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DockerMVC.Controllers {
+    public class HomeController : Controller {
+        public IConfiguration Configuration { get; }
+        public HomeController (IConfiguration configuration) {
+            Configuration = configuration;
+
+        }
+        public IActionResult Index () {
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
+            return View ();
+        }
+
+        public IActionResult About () {
+            ViewData["Message"] = "Your application description page.";
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
+
+            return View ();
+        }
+
+        public IActionResult Contact () {
+            ViewData["Message"] = "Your contact page.";
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
+
+            return View ();
+        }
+
+        public IActionResult Privacy () {
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
+
+            return View ();
+        }
+
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            ViewData["FooterText"] = Configuration.GetValue<string> ("FooterText");
+
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- 2-k8sTutorial/Readme.md
+++ 2-k8sTutorial/Readme.md
@@ -7,3 +7,8 @@ https://medium.com/@fhitchen/adding-heapster-metrics-to-a-kubernetes-dashboard-o
 https://k8smeetup.github.io/docs/tasks/run-application/horizontal-pod-autoscale-walkthrough/
 
 az aks create --resource-group AKSRG --name demo-aks --node-count 3 --dns-name-prefix AKSRG --generate-ssh-keys --node-vm-size Standard_DS5_v2 --kubernetes-version 1.9.9 --no-wait
+
+
+az network public-ip create -g MC_AKSRG_demo-aks_japaneast -n ben-public-ip --allocation-method static --dns-name ben-public-ip
+
+az network public-ip show -g MC_AKSRG_demo-aks_japaneast -n ben-public-ip  --query "{fqdn: dnsSettings.fqdn, address: ipAddress}"
